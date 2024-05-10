d57106bb7f81f1689579c6a880108141dd823106
Damian Jagodziński <damianjagodaz@gmail.com>
2024-05-10 23:20:24 +0200

    task 6.3 problem

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -6,25 +6,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Podaj liczbę wierszy");
-        int rows = reader.nextInt();
-        System.out.println("Podaj liczbę kolumn");
-        int columns = reader.nextInt();
+        int[][] tab = getTab();
 
-        int[][] tab = new int[rows][columns];
+        extracted(tab);
+    }
 
-        for (int i = 0; i < tab.length; i++) {
-            for (int j = 0; j < tab[i].length; j++) {
-                tab[i][j] = i * j;
-            }
-        }
-
+    private static void extracted(int[][] tab) {
         for (int[] row : tab) {
             for (int cell : row) {
                 System.out.printf("%-2d | ", cell);
             }
             System.out.println();
         }
+    }
+
+    private static int[][] getTab() {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Podaj liczbę wierszy");
+        int rows = reader.nextInt();
+        System.out.println("Podaj liczbę kolumn");
+        int columns = reader.nextInt();
+        int[][] tab = new int[rows][columns];
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[i].length; j++) {
+                tab[i][j] = i * j;
+            }
+        }
+        return tab;
     }
 }
